e9148bbde6af9dd249ee1c134e7b713c6905031e
CisLo <54553754+CisLo@users.noreply.github.com>
2021-05-13 00:14:23 +0200

    13/05/2021

--- source/hanoi.c
+++ source/hanoi.c
@@ -8,6 +8,23 @@
 /** TORRES DE HANOI **/
 void torres_hanoi()
 {
-    printf (" Escribe el numero de discos con los que quieres jugar (entre 3 y 10)");
+    int num,n_pos1 = 1,n_pos2 = 2, n_pos3 = 3;
+
+    do {
+        printf (" Escribe el numero de discos con los que quieres jugar (entre 3 y 10)");
+        scanf("%d", &num);
+    } while (num<3 || num>10);
+
+    mover_fichas(num,n_pos1,n_pos2,n_pos3);
+
     //....
+}
+
+void mover_fichas (int num,int n_pos1,int n_pos2, int n_pos3) {
+    
+    mover_fichas(num-1,n_pos1,n_pos3,n_pos2); //
+
+    // printf(" Mou el disc %d de la vareta %c a la vareta %c");
+
+    mover_fichas(num-1,n_pos3,n_pos2,n_pos1); //
 }
